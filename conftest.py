196aeee5f32d826d9f15fa4592b83619d9bc2153
stbelaya <stbelaya@gmail.com>
2023-03-31 11:47:42 +0300

    Создана фикстура Application, реализован запуск и закрытие приложения

---
+++ conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+from fixture.application import Application
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application("C:/Sync/AddressBook/AddressBook.exe")
+    request.addfinalizer(fixture.destroy)
+    return fixture
